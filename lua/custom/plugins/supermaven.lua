8b802b6dd15d2d061077d8901bfdacb5f83fd16a
João Viegas <joao.viegas@rummo.com>
2025-07-16 18:43:49 +0100

    chore: apply formatting

--- lua/custom/plugins/supermaven.lua
+++ lua/custom/plugins/supermaven.lua
@@ -1,26 +1,25 @@
 return {
   {
-    "supermaven-inc/supermaven-nvim",
+    'supermaven-inc/supermaven-nvim',
     config = function()
-      require("supermaven-nvim").setup({
+      require('supermaven-nvim').setup {
         keymaps = {
-          accept_suggestion = "<Tab>",
-          clear_suggestion = "<C-c>",
-          accept_word = "<C-j>",
+          accept_suggestion = '<Tab>',
+          clear_suggestion = '<C-c>',
+          accept_word = '<C-j>',
         },
         ignore_filetypes = { cpp = true }, -- or { "cpp", }
         color = {
-          suggestion_color = "#7A9CA2",
+          suggestion_color = '#7A9CA2',
           cterm = 244,
         },
-        log_level = "info",                -- set to "off" to disable logging completely
+        log_level = 'info', -- set to "off" to disable logging completely
         disable_inline_completion = false, -- disables inline completion for use with cmp
-        disable_keymaps = false,           -- disables built in keymaps for more manual control
+        disable_keymaps = false, -- disables built in keymaps for more manual control
         condition = function()
           return false
-        end -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
-      })
+        end, -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
+      }
     end,
   },
 }
-
