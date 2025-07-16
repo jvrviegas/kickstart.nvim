3e1b813b01da7b752678e73a42f034095baf23fc
João Viegas <joao.viegas@rummo.com>
2025-07-16 18:54:51 +0100

    refactor(toggleterm): update keymap to avoid conflict when typing

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -3,7 +3,7 @@ return {
   version = '*',
   config = function()
     require('toggleterm').setup {
-      open_mapping = [[<leader>tt]],
+      open_mapping = [[<c-\>]],
       direction = 'float',
       float_opts = {
         border = 'curved',
