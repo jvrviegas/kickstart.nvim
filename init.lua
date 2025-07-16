72dcee4b1d87b481ffe39c06b50c7bcbe0f1a6e6
João Viegas <joao.viegas@rummo.com>
2025-07-16 18:45:37 +0100

    refactor(init.lua): add eslint to lsp servers

--- init.lua
+++ init.lua
@@ -720,6 +720,7 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         ts_ls = {},
+        eslint = {},
         --
 
         lua_ls = {
